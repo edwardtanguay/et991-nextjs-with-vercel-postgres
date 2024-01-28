b0787227df8c235179db2191859711da6e92b5a9
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-28 22:25:53 +0100

    sql online

--- src/app/api/sql/route.ts
+++ src/app/api/sql/route.ts
@@ -1,12 +1,13 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request: Request) {
   try {
+    if (process.env.APP_ENV !== 'development') throw Error('not authorized');
     const result =
       await sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
     return NextResponse.json({ result }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+  } catch (error: any) {
+    return NextResponse.json(error.message, { status: 500 });
   }
 }

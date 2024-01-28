1812ca39dbcb6a408258f5515ff73d0b2103db7b
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-28 22:50:35 +0100

    create notes ok

--- src/app/api/sql/route.ts
+++ src/app/api/sql/route.ts
@@ -2,12 +2,17 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  try {
-    if (process.env.APP_ENV !== 'development') throw Error('not authorized');
-    const result =
-      await sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
-    return NextResponse.json({ result }, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json(error.message, { status: 500 });
-  }
+	try {
+		if (process.env.APP_ENV !== 'development') throw Error('not authorized');
+
+		// CREATE PETS
+		// await sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
+
+		// CREATE NOTES
+		await sql`CREATE TABLE IF NOT EXISTS Notes ( id SERIAL PRIMARY KEY, body varchar(255), rank decimal);`;
+
+		return NextResponse.json("ok", { status: 200 });
+	} catch (error: any) {
+		return NextResponse.json(error.message, { status: 500 });
+	}
 }

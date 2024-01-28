10e91a7d7a9512d3bab56fcaf140563d44ad5829
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-28 23:10:52 +0100

    add-pin out

--- src/app/api/get-pin/route.ts
+++ src/app/api/get-pin/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
 	// return NextResponse.json({ pin: process.env.APP_PIN }, { status: 200 });
-	return NextResponse.json({ pin: process.env.APP_PIN}, { status: 200 });
+	// return NextResponse.json({ pin: process.env.APP_PIN}, { status: 200 });
 }

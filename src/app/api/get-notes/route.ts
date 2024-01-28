4c03bca80cf4f0cd6f0ab6c6e97f2be569bc317d
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-29 00:30:23 +0100

    form finished

--- src/app/api/get-notes/route.ts
+++ src/app/api/get-notes/route.ts
@@ -2,6 +2,6 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-	const result = await sql`SELECT * FROM Notes;`;
-	return NextResponse.json({ result }, { status: 200 });
+	const result = await sql`SELECT * FROM Notes ORDER BY Rank DESC;`;
+	return NextResponse.json( result.rows , { status: 200 });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,61 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseUrl = "http://localhost:3051";
+
+interface note {
+	id: number;
+	body: string;
+	rank: number;
+}
+
+interface newNote {
+	body: string;
+	rank: string;
+	pin: string;
+}
+
+const blankNewNote = {
+	body: "",
+	rank: 3.5,
+	pin: "",
+};
+
 export default function Home() {
+	const [notes, setNotes] = useState<note[]>([]);
+	const [newNote, setNewNote] = useState<newNote>(blankNewNote);
+
+	useEffect(() => {
+		(async () => {
+			const headers = {
+				"Cache-Control": "no-cache",
+				Pragma: "no-cache",
+				Expires: "0",
+			};
+			const response = await axios.get(`${baseUrl}/api/get-notes`, {
+				headers,
+			});
+			const _notes = response.data;
+			setNotes(_notes);
+		})();
+	}, []);
+
+	const handleFieldChange = (fieldIdCode: string, value: string) => {
+		switch (fieldIdCode) {
+			case "body":
+				newNote.body = value;
+				break;
+			case "rank":
+				newNote.rank = value;
+				break;
+			case "pin":
+				newNote.pin = value;
+				break;
+		}
+		setNewNote(structuredClone(newNote));
+	};
+
 	return (
 		<main className="p-6">
 			<h1 className="text-2xl mb-4">Next.js with Vercel Postgres</h1>
@@ -8,31 +65,84 @@ export default function Home() {
 					<label className="block mb-1" htmlFor="note">
 						Note:
 					</label>
-					<textarea className="w-full" id="note" />
+					<textarea
+						value={newNote.body}
+						onChange={(e) =>
+							handleFieldChange("body", e.target.value)
+						}
+						spellCheck={false}
+						className="w-full"
+						id="note"
+					/>
 				</div>
 
 				<div className="mb-3">
 					<label className="block mb-1" htmlFor="rank">
 						Rank:
 					</label>
-					<input className="w-[3rem] text-right" type="text" id="rank" />
-					<div className="text-xs mt-1 text-slate-700">0 = not important, 5 very important, e.g 4.3</div>
+					<input
+						value={newNote.rank}
+						onChange={(e) =>
+							handleFieldChange("rank", e.target.value)
+						}
+						className="w-[3rem] text-right"
+						type="text"
+						id="rank"
+					/>
+					<div className="text-xs mt-1 text-slate-700">
+						0 = not important, 5 very important, e.g 4.3
+					</div>
 				</div>
-				
+
 				<div className="mb-3">
-					<label className="block mb-1" htmlFor="apppin">
+					<label className="block mb-1" htmlFor="pin">
 						PIN
 					</label>
-					<input className="w-[8rem]" type="password" id="rank" />
-					<div className="text-xs mt-1 text-slate-700">enter access pin</div>
+					<input
+						value={newNote.pin}
+						onChange={(e) =>
+							handleFieldChange("pin", e.target.value)
+						}
+						className="w-[8rem]"
+						type="password"
+						id="pin"
+					/>
+					<div className="text-xs mt-1 text-slate-700">
+						enter access pin
+					</div>
 				</div>
-				
+
 				<div className="mt-6 flex justify-end">
-					<button className="bg-slate-700 px-2 py-1 rounded text-slate-400">Save</button>
+					<button className="bg-slate-700 px-2 py-1 rounded text-slate-400">
+						Save
+					</button>
 				</div>
-
-
 			</form>
+
+			<div>body: [{newNote.body}]</div>
+			<div>rank: [{newNote.rank}]</div>
+			<div>pin: [{newNote.pin}]</div>
+
+			<section className="mt-6">
+				<h2 className="text-1xl mb-4">
+					There are {notes.length} notes:
+				</h2>
+				<ul>
+					{notes.map((note) => {
+						return (
+							<li
+								className="text-blue-950 flex gap-2 mb-3"
+								key={note.id}
+							>
+								<span className="text-yellow-500 bg-slate-600 h-fit w-[1.6rem] p-1 rounded text-xs flex justify-center">
+									{Number(note.rank).toFixed(1)}
+								</span>
+								{note.body}
+							</li>
+						);
+					})}
+				</ul>
+			</section>
 		</main>
 	);
 }
